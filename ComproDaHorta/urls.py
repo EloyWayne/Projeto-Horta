fca6d04d938a252b9f1e05965a0fe5ae1f1e64ed
Wayne Eloy <138615880+EloyWayne@users.noreply.github.com>
2023-11-07 19:07:21 -0300

    Directory Url

--- ComproDaHorta/urls.py
+++ ComproDaHorta/urls.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
-from django.urls import path
-from django.conf.urls import include
+from django.urls import path, include
 
 urlpatterns = [
     path('grappelli/', include('grappelli.urls')),
     path('admin/', admin.site.urls),
+    path('',include('apps.produtos.urls'))
 ]
--- apps/produtos/views.py
+++ apps/produtos/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from .models import Pedido
 
-# Create your views here.
+def PedidoView(request):
+    view_lista = Pedido.objects.all()
+    return render(request, 'core.html', {'Pedido': view_lista})
