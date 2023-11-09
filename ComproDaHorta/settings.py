bae795433e0590dde12f51bb1970f673aba6a079
Wayne Eloy <138615880+EloyWayne@users.noreply.github.com>
2023-11-09 18:04:28 -0300

    agora vai

--- ComproDaHorta/settings.py
+++ ComproDaHorta/settings.py
@@ -121,7 +121,7 @@ USE_TZ = True
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # CONFIGURAÇÃO DOS ARQUIVOS ESTÁTICOS
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static")
